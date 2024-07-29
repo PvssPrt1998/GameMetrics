24b89b42f3c64b4fe03afb472ec6db84fa4d5794
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-07-29 15:25:05 +0500

    Initial Commit

---
+++ GameMetrics/GameMetricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameMetricsApp.swift
+//  GameMetrics
+//
+//  Created by Николай Щербаков on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameMetricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
