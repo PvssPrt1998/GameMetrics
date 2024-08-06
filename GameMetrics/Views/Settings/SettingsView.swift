3557c8bcf1f2e0d56240591c542d8491b7be2dba
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-08-06 15:43:38 +0500

    Added webview for terms of use, privacy, license

--- GameMetrics/Views/Settings/SettingsView.swift
+++ GameMetrics/Views/Settings/SettingsView.swift
@@ -2,79 +2,101 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @State var showTermsOfUse = false
+    @State var showPrivacy = false
+    @State var showLicense = false
+    
     var body: some View {
-        VStack(spacing: 25) {
-            GrabberView()
-                .padding(EdgeInsets(top: 5, leading: 0, bottom: 18, trailing: 0))
-            TextCustom(text: "Settings", size: 34, weight: .bold, color: .textMain)
-                .padding(EdgeInsets(top: 3, leading: 16, bottom: 8, trailing: 16))
-                .frame(maxWidth: .infinity, alignment: .leading)
-            VStack(spacing: 0){
-                VStack(spacing: 0) {
-                    Button {
-                        //action contact
-                    } label: {
-                        HStack(spacing: 12) {
-                            Image(systemName: "bubble.fill")
-                                .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
-                            TextCustom(text: "Contact us", size: 16, weight: .bold, color: .settingsText)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+        ZStack {
+            VStack(spacing: 25) {
+                GrabberView()
+                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 18, trailing: 0))
+                TextCustom(text: "Settings", size: 34, weight: .bold, color: .textMain)
+                    .padding(EdgeInsets(top: 3, leading: 16, bottom: 8, trailing: 16))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                VStack(spacing: 0){
+                    VStack(spacing: 0) {
+                        Button {
+                            //action contact
+                        } label: {
+                            HStack(spacing: 12) {
+                                Image(systemName: "bubble.fill")
+                                    .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                                TextCustom(text: "Contact us", size: 16, weight: .bold, color: .settingsText)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                            }
                         }
+                        .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
+                        Divider()
                     }
-                    .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
-                    Divider()
-                }
-                VStack(spacing: 0) {
-                    Button {
-                        //action contact
-                    } label: {
-                        HStack(spacing: 12) {
-                            Image(systemName: "wallet.pass.fill")
-                                .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
-                            TextCustom(text: "License", size: 16, weight: .bold, color: .settingsText)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                    VStack(spacing: 0) {
+                        Button {
+                            showLicense = true
+                        } label: {
+                            HStack(spacing: 12) {
+                                Image(systemName: "wallet.pass.fill")
+                                    .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                                TextCustom(text: "License", size: 16, weight: .bold, color: .settingsText)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                            }
                         }
+                        .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
+                        Divider()
                     }
-                    .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
-                    Divider()
-                }
-                VStack(spacing: 0) {
-                    Button {
-                        //action contact
-                    } label: {
-                        HStack(spacing: 12) {
-                            Image(systemName: "menucard.fill")
-                                .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
-                            TextCustom(text: "Terms of use", size: 16, weight: .bold, color: .settingsText)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                    VStack(spacing: 0) {
+                        Button {
+                            showTermsOfUse = true
+                        } label: {
+                            HStack(spacing: 12) {
+                                Image(systemName: "menucard.fill")
+                                    .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                                TextCustom(text: "Terms of use", size: 16, weight: .bold, color: .settingsText)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                            }
                         }
+                        .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
+                        Divider()
                     }
-                    .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
-                    Divider()
-                }
-                VStack(spacing: 0) {
-                    Button {
-                        //action contact
-                    } label: {
-                        HStack(spacing: 12) {
-                            Image(systemName: "shield.fill")
-                                .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
-                            TextCustom(text: "Privacy", size: 16, weight: .bold, color: .settingsText)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                    VStack(spacing: 0) {
+                        Button {
+                            showPrivacy = true
+                        } label: {
+                            HStack(spacing: 12) {
+                                Image(systemName: "shield.fill")
+                                    .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                                TextCustom(text: "Privacy", size: 16, weight: .bold, color: .settingsText)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .fontCustom(size: 20, weight: .semibold, color: .specialPrimary)
+                            }
                         }
+                        .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
                     }
-                    .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10))
                 }
+                
             }
-            
+            if showTermsOfUse {
+                SettingsWebView(action: {
+                    showTermsOfUse = false
+                }, url: "https://www.google.com/")
+            }
+            if showPrivacy {
+                SettingsWebView(action: {
+                    showPrivacy = false
+                }, url: "https://www.google.com/")
+            }
+            if showLicense {
+                SettingsWebView(action: {
+                    showLicense = false
+                }, url: "https://www.google.com/")
+            }
         }
     }
 }
